23dfdb32f1719125281b2cb00a4e4856dca542ce
Xiangkun Yang <vipakun@hotmail.com>
2016-08-07 21:50:41 -0500

    morgan middleware added.

--- server/server.js
+++ server/server.js
@@ -2,13 +2,40 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var _ = require('lodash');
+var morgan = require('morgan');
 
+var lions = [];
+var id = 0;
+
+var updateId = function(req, res, next) {
+  if (!req.body.id) {
+    id++;
+    req.body.id = id + '';
+  }
+  next();
+};
+
+app.use(morgan('dev'))
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var lions = [];
-var id = 0;
+app.use(function(err, req, res, next) {
+  if (err) {
+    res.status(500).send(error);
+  }
+});
+
+app.param('id', function(req, res, next, id) {
+  var lion = _.find(lions, {id: id});
+
+  if (lion) {
+    req.lion = lion;
+    next();
+  } else {
+    res.send();
+  }
+});
 
 app.get('/lions', function(req, res){
   res.json(lions);
@@ -42,6 +69,12 @@ app.put('/lions/:id', function(req, res) {
   } else {
     var updatedLion = _.assign(lions[lion], update);
     res.json(updatedLion);
+  }
+});
+
+app.use(function(err, req, res, next) {
+  if(err) {
+    res.status(500).send(error);
   }
 });
 
